a2a0e9cbddb98ea1053c5984f1ec9a9eee257c93
Simon Dirmeier <simd23@protonmail.com>
2022-04-10 16:32:27 +0200

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clad
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clad/core_test.clj
@@ -0,0 +1,7 @@
+(ns cladd.core-test
+  (:require [clojure.test :refer :all]
+            [cladd.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
