b4987acdf7f6fc54b850d3f51c694d74957de931
Simon Dirmeier <simd23@protonmail.com>
2022-04-19 22:14:30 +0200

    Implement logpdf of normal

--- src/clad/core.clj
+++ src/clad/core.clj
@@ -1,6 +1,11 @@
-(ns cladd.core)
+(ns clad.core)
+(require '[clad.node :as node])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn f ^double [^double y ^double mu ^double sigma]
+  (- (/ (Math/pow (- y mu) 2) (* -2.0 (Math/pow sigma 2)))
+     (Math/log sigma)
+     (/ (Math/log (* 2.0 Math/PI)) 2.0)))
+
+(defn -main
+  [& args]
+  (print (f 0 2 3)))
