bbd84737e2fc52ffd15858d49b4e5bc12a25be06
Simon Dirmeier <simd23@protonmail.com>
2022-04-24 00:26:23 +0200

    Add computation of adjoints

--- src/clad/grad.clj
+++ src/clad/grad.clj
@@ -1,8 +1,11 @@
 (ns clad.grad)
 (require '[clad.expr :as expr])
+(require '[clad.node :as node])
 
 (defn grad [f idx]
-  (let [graph (expr/expression-graph f)]
-    (println 1123123131)
+  (let [graph (expr/expression-graph f)
+        adj (node/adjacency graph)]
+    (println "------------")
+    (println adj)
     (doseq [item graph]
       (println item))))
--- src/clad/node.clj
+++ src/clad/node.clj
@@ -1,4 +1,5 @@
-(ns clad.node)
+(ns clad.node
+  (:require [clojure.core.matrix :as m]))
 
 (def counter (atom -1))
 
@@ -8,4 +9,18 @@
 (defrecord node [idx op parent children adjoint value])
 
 (defn create-node [op parent children adjoint value]
-  (node. (next-value) op parent children adjoint value))
+  (node. (int (next-value)) op parent children adjoint value))
+
+
+(defn adjacency [graph]
+  (let [n-nodes (+ (reduce max (map (fn [element] (:idx element)) graph)) 1)]
+    (loop [nodes graph adj (m/new-matrix n-nodes n-nodes)]
+      (if (empty? nodes)
+        adj
+        (let [[part & remaining] nodes]
+          (recur
+            (if (nil? (:parent part)) remaining (conj remaining (:parent part)))
+            (if
+              (nil? (:idx (:parent part)))
+              adj
+              (m/mset adj (:idx (:parent part)) (:idx part) 1))))))))
