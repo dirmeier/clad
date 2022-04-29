58051b1eb67a91dfa1ea943f45f4589dddcefe95
Simon Dirmeier <simd23@protonmail.com>
2022-04-29 20:58:57 +0200

    Implement topological sort

--- src/clad/node.clj
+++ src/clad/node.clj
@@ -1,5 +1,6 @@
 (ns clad.node
-  (:require [clojure.core.matrix :as m]))
+  (:require [clojure.core.matrix :as m])
+  (:require [clojure.set :as set]))
 
 (def counter (atom -1))
 
@@ -40,29 +41,49 @@
           (assoc new-nodes (:idx part) {:op (:op part) :adjoint (:adjoint part) :value (:value part)})
           )))))
 
-; do again: filter ndoes topologically, then go upp
-(defn bottom-up [graph]
-  (let [adj (adj graph)] (loop [nodes (:nodes graph) new-graph []]
-     (if
-       (empty? nodes)
-       {:adj adj :nodes (set new-graph)}
-       (let [[part & remaining] nodes]
-         (recur
-           (if (nil? (:parent part))
-             remaining
-             (conj remaining
-                   (get (m/get-column adj (:idx part)))
-                   ;(:parent part)
+(defn adj-to-edge-map [graph]
+  (let [adj (:adj graph)
+        m (m/row-count adj)
+        n (m/column-count adj)]
+    (loop [i 0 parent-node-map {}]
+      (if (= i m)
+        parent-node-map
+        (recur
+          (+ i 1)
+          (loop [j 0 parent-node-row []]
+            (if (= j n)
+              (assoc parent-node-map i parent-node-row)
+              (recur
+                (+ j 1)
+                (if (= (m/mget adj i j) 1) (conj parent-node-row j) parent-node-row)))))))))
 
-                   ))
-           (if
-             (nil? (:value part))
-             (conj new-graph
-                   (assoc part :value
-                               (reduce
-                                 (get-op (:op part))
-                                 (map
-                                   (fn [child] (:value child))
-                                   (:children part)
-                                   ))))
-             (conj new-graph part))))))))
+(defn in? [coll elm] (some #(= elm %) coll))
+
+(defn filter-map [lhs rhs]
+  (loop [keys rhs arr []]
+    (if (empty? keys)
+      arr
+      (let [[par & remaining] keys]
+        (recur
+          remaining
+          (if (in? lhs par) (conj arr par))
+          )))))
+
+(defn topological-sort [graph]
+  (let [edge-map (adj-to-edge-map graph)]
+    (loop [node-topological [] current-edge-map edge-map]
+      (if (empty? current-edge-map)
+        node-topological
+        (let [nodes (filter #(= (count (get current-edge-map %)) 0) (keys current-edge-map)) ]
+          (recur
+            (into node-topological nodes)
+            (let [
+                  current-edge-map (apply dissoc current-edge-map nodes)
+                  keys (keys current-edge-map)]
+              (loop [curk keys curm current-edge-map]
+                (if (empty? curk)
+                  curm
+                  (let [[part & remaining] curk]
+                    (recur
+                      remaining
+                      (assoc curm part (filter-map (get curm part) keys)))))))))))))
