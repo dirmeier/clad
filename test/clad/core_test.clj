111fa8f3d80530f08e079099e6971fd345e75b34
Simon Dirmeier <simd23@protonmail.com>
2022-05-04 21:38:04 +0200

    Add unit tests

--- test/clad/core_test.clj
+++ test/clad/core_test.clj
@@ -1,7 +1,26 @@
 (ns clad.core-test
   (:require [clojure.test :refer :all]
-            [cladd.core :refer :all]))
+            [clad.core :refer [grad]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn log-pdf ^double [^double y ^double mu ^double sigma]
+  (- (/ (Math/pow (- y mu) 2) (* -2.0 (Math/pow sigma 2)))
+     (Math/log sigma)
+     (/ (Math/log (* 2.0 Math/PI)) 2.0)))
+;
+;(defn tanh ^double [^double x ^double y]
+;  (/ (1.0 - (Math/exp (- x)))
+;     (1.0 + (Math/exp (- y)))))
+
+(def log-pdf "(* (* 2.0 Math/PI) 7.0)")
+
+(defn close-to?
+  [x y epsilon]
+  (<= (abs (- x y)) epsilon))
+
+(deftest test-normal-logpdf-gradients
+  (testing "gradient w.r.t y"
+    (is (close-to? (grad log-pdf 0) 21.991148575128552  0.001)))
+  (testing "gradient w.r.t mu"
+    (is (close-to? (grad log-pdf 1) 14.0  0.001)))
+  (testing "gradient w.r.t sigma"
+    (is (close-to? (grad log-pdf 2) 6.283185307179586  0.001))))
